f9551f23cd43e8ea3904f939c8dc43f4b6999ed4
muzing <muzi2001@foxmail.com>
2024-04-16 23:09:07 +0800

    Add project files

---
+++ Data/ModelParameters.m
@@ -0,0 +1,74 @@
+% 模型数据
+% 由于尚处模型早期开发阶段，大量模型中的参数仍需调试修改，暂通过m脚本方式
+% 保存数据；后续考虑将数据从基础工作区移动至模型工作区中。
+
+%% 电池与电机子系统
+
+BatteryData.ChrgMax = -50000;  % 电池最大充电功率，(W)
+BatteryData.DischrgMax = 50000;  % 电池最大放电功率，(W)
+
+%% 传动系子系统
+
+% 轴连接器
+axle_k = 5000;  % 扭转刚度，(N*m/rad)
+axle_b = 10;  % 扭转阻尼，(N*m*s/rad)
+bevAxle_wc = 300;  % 阻尼截止频率，(rad/s)
+
+% 开放式差速器
+OpenDifferential.TransRatio = 37/7;  % 主减速器传动比
+OpenDifferential.Efficiency = 0.98;  % 传动效率因数（常量固定值）
+OpenDifferential.CarrierInertia = 0.025;  % 输入轴转动惯量，(kg*m²)
+OpenDifferential.CarrierDamping = 1e-3;  % 输入轴阻尼，(N*m*s/rad)
+OpenDifferential.AxleInertia = 0.01;  % 输出轴转动惯量，(kg*m²)
+OpenDifferential.AxleDamping = 1e-3;  % 输出轴阻尼，(N*m*s/rad)
+
+%% 车轮车身子系统
+
+% -----------------整车------------------------
+
+VehicleData.Wheelbase = 4.390;  % 轴距，(m)
+VehicleData.TrackwidthF = 1.918;  % 前轴轮距，(m)
+VehicleData.TrackwidthR = 1.958;  % 后轴轮距，(m)
+VehicleData.FrontalArea = 4.64;  % 迎风面积，(m²)
+VehicleData.Cd = 0.372;  % 风阻系数
+
+% 空载
+VehicleData.Mass.NoLoad.Mass = 4617;  % 空载质量，(kg)
+VehicleData.Mass.NoLoad.a = 2.667;  % 空载时质心到前轴距离，(m)
+VehicleData.Mass.NoLoad.b = VehicleData.Wheelbase - ...
+    VehicleData.Mass.NoLoad.a;  % 空载时质心到后轴距离，(m)
+VehicleData.Mass.NoLoad.hg = 0.895;  % 空载时质心高度，(m)
+VehicleData.Mass.NoLoad.Iyy = 3500;  % *空载绕y轴转动惯量，(kg*m²)
+
+% 满载
+VehicleData.Mass.Full.Mass = 4617;  % 满载质量，(kg)
+VehicleData.Mass.Full.a = 2.667;  % 满载时质心到前轴距离，(m)
+VehicleData.Mass.Full.b = VehicleData.Wheelbase - ...
+    VehicleData.Mass.Full.a;  % 满载时质心到后轴距离，(m)
+VehicleData.Mass.Full.hg = 0.910;  % 满载时质心高度，(m)
+VehicleData.Mass.Full.Iyy = 3500;  % *满载绕y轴转动惯量，(kg*m²)
+
+% -----------------车轮------------------------
+
+VehicleData.Wheel.LoadedRadius = 0.357;  % 负载半径，(m)
+VehicleData.Wheel.UnloadedRadius = 0.370;  % 无负载半径，(m)
+VehicleData.Wheel.RelaxationLength = 0.5;  % *松弛长度，(m)
+VehicleData.Wheel.AxleViscousDampingCoefficient = 0.001;  % *轴粘滞阻尼系数，(N*m*s/rad)
+VehicleData.Wheel.Inertia = 0.8;  % *转动惯量，(kg*m²)
+VehicleData.Wheel.Pressure = 98 * 6894.75729;  % 胎压，(Pa)
+% *魔术公式
+VehicleData.Wheel.Dx = 1;
+VehicleData.Wheel.Cx = 1.65;
+VehicleData.Wheel.Bx = 10;
+VehicleData.Wheel.Ex = 0.01;
+
+% -----------------制动器------------------------
+
+%% 环境
+
+EnvironmentData.AirTemp = 273.15 + 20;  % 环境温度，(k)
+EnvironmentData.GravitationalAcceleration = 9.81;  % 重力加速度，(m/s²)
+
+%% 写入模型工作区
+
+% mdlWrkSps = get_param("Dual_Clutch_Trans", 'modelworkspace');
---
+++ Data/motorPlot.m
@@ -0,0 +1,113 @@
+% 电机数据处理与绘图
+
+%% 加载数据
+load("MotorData.mat");
+
+%% 外特性曲线
+
+motSpeed = MotorData.ExternalCharacteristics.Rated.rpm;
+ratedTorque = MotorData.ExternalCharacteristics.Rated.torque;
+ratedPower = MotorData.ExternalCharacteristics.Rated.power;
+peakTorque = MotorData.ExternalCharacteristics.Peak.torque;
+peakPower = MotorData.ExternalCharacteristics.Peak.power;
+
+figure('Name', "大轿车电机外特性曲线")
+
+hold on
+
+xlabel("转速 (rpm)")
+xlim([0, max(motSpeed)+400])
+xticks(0:800:max(motSpeed))
+
+yyaxis left
+plot(motSpeed, ratedTorque, "-", 'DisplayName', "额定转矩")
+plot(motSpeed, peakTorque, "--", 'DisplayName', "峰值转矩")
+ylabel("转矩 (N·m)")
+ylim([0, 480])
+yticks(0:40:480)
+
+yyaxis right
+plot(motSpeed, ratedPower, "-", 'DisplayName', "额定功率")
+plot(motSpeed, peakPower, "--", 'DisplayName', "峰值功率")
+ylabel("功率 (kW)")
+ylim([0, 260])
+yticks(0:20:260)
+
+grid on
+legend('Location', 'best')
+title("电机外特性曲线图")
+
+hold off
+
+clear peakPower peakTorque ratedPower ratedTorque motSpeed
+
+%% 效率 MAP 图（电动）
+
+motSpdArray = MotorData.Efficiency.RawData.Drive.speed;
+motTorArray = MotorData.Efficiency.RawData.Drive.torque;
+motEffArray = MotorData.Efficiency.RawData.Drive.systemEfficiency;
+
+% 设置网格与插值
+[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray),...
+    min(motTorArray)-20:2:max(motTorArray)+20);
+motEff = griddata(motSpdArray, motTorArray, motEffArray, gridX, gridY);
+
+% 根据具体情况调整等高线层级与标签层级
+levelList = [70, 75, 80, 83, 86, 87:1:91, 91:0.5:95];
+labelLevelList = [70, 75, 80, 83, 86, 88, 90:1:93, 93:0.5:95];
+
+figure("Name","电机系统效率MAP图（电动）")
+colormap jet
+
+[C,h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
+clabel(C, h, labelLevelList, 'LabelSpacing', 200)
+
+title("电机系统效率 MAP 图（电动）")
+xlabel("转速(rpm)")
+ylabel("转矩(N·m)")
+
+% clear motSpdArray motTorArray motEffArray gridX gridY
+% clear motEff levelList labelLevelList C h
+
+%% 效率 MAP 图（馈电）
+motSpdArray = MotorData.Efficiency.RawData.Feed.speed;
+motTorArray = MotorData.Efficiency.RawData.Feed.torque;
+motEffArray = MotorData.Efficiency.RawData.Feed.systemEfficiency;
+
+% 设置网格与插值
+[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray),...
+    min(motTorArray)-20:1:max(motTorArray)+20);
+motEff = griddata(motSpdArray, motTorArray, motEffArray, gridX, gridY);
+
+% 根据具体情况调整等高线层级
+levelList = [70, 75, 80, 83, 86, 88:1:90, 91:0.5:93.5, 94, 95];
+labelLevelList = [70, 75, 80, 83, 86, 88:1:93, 94, 95];
+
+figure("Name","电机系统效率MAP图（馈电）")
+colormap jet
+
+[C,h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
+clabel(C, h, labelLevelList, 'LabelSpacing', 200)
+
+title("电机系统效率 MAP 图（馈电）")
+xlabel("转速(rpm)")
+ylabel("转矩(N·m)")
+
+clear motSpdArray motTorArray motEffArray gridX gridY
+clear motEff levelList labelLevelList C h
+
+%% 数据处理（用于建模）
+% 以效率数据为依据，将电驱馈电整合，并以边界作为外特性，得到整理后的数据，
+% 用于 MappedMotor 模块的参数输入
+
+% 外特性（与直接读取peek有微小区别）
+motSpdArray = unique(MotorData.Efficiency.RawData.Drive.speed)';
+motTorArray = zeros(1, length(motSpdArray));
+
+for index = 1:length(motSpdArray)
+    motTorArray(index) = max(MotorData.Efficiency.RawData.Drive{...
+        MotorData.Efficiency.RawData.Drive.speed == motSpdArray(index), 5});
+end
+
+figure("Name","MappedMotor 建模数据")
+plot(motSpdArray, motTorArray)
