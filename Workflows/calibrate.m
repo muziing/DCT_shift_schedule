cdc253fe86c1f8de8ef49133d5f33920a6048a58
muzing <muzi2001@foxmail.com>
2024-04-20 16:08:36 +0800

    Update `calibrate`
    - 根据模型更新，调整了编程控制AP开度、挡位常量的方式；
    - 其他更新优化，发现一批已知问题，暂未解决，使用 `TODO` 注释标记

--- Workflows/calibrate.m
+++ Workflows/calibrate.m
@@ -13,22 +13,23 @@
 
 %% 配置
 
-accelPedalValues = [10, 40, 50, 90, 100]; % 加速踏板开度列表
+accelPedalValues = [15, 30, 50, 90, 100] / 100; % 加速踏板开度列表
 gearNumbers = 1:4; % 挡位数
 delaySpeed = 5; % 换挡延迟，(km/h)
 delaySpeedFactor = 1; % 换挡延迟系数
 
 modelName = "Dual_Clutch_Trans"; % Simulink 模型文件名（不含扩展名）
-simStopTime = "300"; % 仿真停止时间，足以让车速达到稳定即可
+DrivingCycle = "AccelTest"; % 选择驾驶循环
+simStopTime = "400"; % 仿真停止时间，足以让车速达到稳定即可
 
 % 仿真模式，['normal', 'accelerator', 'rapid-accelerator'] 之一
 simMode = "accelerator";
 
 % 加速度截止值，当加速度绝对值小于此值时认为车速已经稳定，舍弃此时刻后面的数据
-accelThreshold = 0.01;
+accelThreshold = 0.05;
 
-% 根据经验，仿真开始的一小段时间内求解尚未完全稳定，容易出现值突变，舍去
-startIndex = 1001;
+% 根据经验，仿真开始的一小段时间内求解尚未完全稳定，容易出现值突变，舍去（待优化）
+startIndex = 2501;
 
 %% 创建、配置并运行仿真任务
 
@@ -41,11 +42,19 @@ index = 1;
 % 为每个仿真任务设置不同的AP开度与挡位
 for apIndex = 1:length(accelPedalValues)
     for gear = gearNumbers
+
+        % 启用模型的标定调试模式
+        simIn(index) = simIn(index).setVariable('DrivingCycle', "AccelTest", ...
+            'Workspace', modelName);
+        simIn(index) = simIn(index).setVariable('CalibrateMode', true, ...
+            'Workspace', modelName);
+
         % 配置AP开度与挡位
+
         simIn(index) = simIn(index).setBlockParameter( ...
-            modelName + "/GearConstant", 'Value', num2str(gear));
+            modelName + "/TCU/ConstantGear/GearConstant", 'Value', num2str(gear));
         simIn(index) = simIn(index).setBlockParameter( ...
-            modelName + "/AccelConstant", 'Value', ...
+            modelName + "/Driver/AccelCmdGain", 'Gain', ...
             num2str(accelPedalValues(apIndex)));
 
         % 配置其他仿真参数
@@ -76,9 +85,10 @@ for apIndex = 1:length(accelPedalValues)
         timestamps = velocityTimeTable.Time;
         
         % 因差分会显著放大噪声，使用移动平均值对速度简单滤波
-        smoothedVelocities = smooth(velocityTimeTable.Data, 'moving', 3);
+        smoothedVelocities = smooth(velocityTimeTable.Data, 'moving', 20);
         % 差分得到加速度
         accelerations = diff(smoothedVelocities) ./ diff(seconds(timestamps));
+        jerks = diff(accelerations) ./ diff(seconds(timestamps(1:end-1)));
 
         % 获取电机转速转矩，查效率MAP图获取对应工作点效率
         % motorSpds = get(simOut(index).logsout, "MotorSpeed").Values.Data;
@@ -88,7 +98,10 @@ for apIndex = 1:length(accelPedalValues)
         %     MotorData.Efficiency.Drive.Eff, ...
         %     motorSpds, motorTrqs); % 通过插值获取电机效率值
 
+        % TODO 将起始时间处理为驾驶员踩满加速踏板后的时刻
+
         % 截取求解已稳定、车速已稳定（加速度很小、且不再超过阈值）之间的数据
+        % TODO 优化末尾截断方法，以跃度突变作为截断标志，而不只是加速度值很小
         endIndex = get_consec_below_thold(abs(accelerations), accelThreshold);
         if isnan(endIndex)
             warning("未能确定车速稳定时刻，请检查模型、仿真时间与加速度阈值！")
@@ -97,10 +110,12 @@ for apIndex = 1:length(accelPedalValues)
         timestamps = timestamps(startIndex:endIndex);
         smoothedVelocities = smoothedVelocities(startIndex:endIndex);
         accelerations = accelerations(startIndex:endIndex);
+        jerks = jerks(startIndex:endIndex);
         % motorEffs = motorEffs(startIndex:endIndex);
         
         % plot(timestamps, smoothedVelocities) % 【车速-时间】曲线，调试用
         % plot(timestamps, accelerations) % 【加速度-时间】曲线，调试用
+        plot(timestamps, jerks)
         
         % 将处理后的数据重新整理至一张 timetable 中
         dataTable = timetable(smoothedVelocities, accelerations, ...
@@ -109,7 +124,7 @@ for apIndex = 1:length(accelPedalValues)
         % 通过一维插值，计算【加速度-车速】、【电机效率-车速】曲线
         equispacedVelocities = 0:0.001:max(smoothedVelocities);
         accel2velocitie = interp1(smoothedVelocities, accelerations, ...
-            equispacedVelocities);
+            equispacedVelocities); % TODO 解决可能存在相同车速的导致采样点不唯一的问题
         % eff2velocitie = interp1(smoothedVelocities, motorEffs, ....
         %     equispacedVelocities);
 
@@ -125,8 +140,8 @@ for apIndex = 1:length(accelPedalValues)
     end
 end
 
-clear index apIndex velocityTimeTable 
-clear timestamps smoothedVelocities accelerations endIndex
+clear index apIndex velocityTimeTable
+% clear timestamps smoothedVelocities accelerations endIndex
 clear equispacedVelocities accel2velocitie
 % clear motorSpds motorTrqs motorEffs
 
@@ -139,7 +154,7 @@ index = 1;
 
 for apIndex = 1:length(accelPedalValues)
     
-    figure("Name", num2str(accelPedalValues(apIndex))+"%")
+    figure("Name", num2str(accelPedalValues(apIndex)*100)+"%")
     hold on
 
     for gear = gearNumbers
@@ -148,9 +163,9 @@ for apIndex = 1:length(accelPedalValues)
             'DisplayName', num2str(gear)+"挡")
     end
 
-    xlabel("车速")
-    ylabel("加速度")
-    title("加速踏板开度："+num2str(accelPedalValues(apIndex))+"%")
+    xlabel("车速 / (km/h)")
+    ylabel("加速度 / (m/s^2)")
+    title("加速踏板开度："+num2str(accelPedalValues(apIndex)*100)+"%")
     legend('Location', 'best')
 
     hold off
