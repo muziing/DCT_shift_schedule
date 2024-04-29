1e70164cb3cf652f9fec6c7e456c45788a9af031
muzing <muzi2001@foxmail.com>
2024-04-29 22:05:09 +0800

    Update `simin_factory`
    更新工厂函数 `simin_factory`：
    - 将入参限制为 ShiftSchedule 类的实例，并对应修改属性名
    - 使支持向量化操作，可传入换挡规律数组

--- Workflows/simin_factory.m
+++ Workflows/simin_factory.m
@@ -1,50 +1,62 @@
 function simIn = simin_factory(shiftSchedule, drivingCycle)
 %SIMIN_FACTORY 工厂函数，用于生成仿真任务对象
+%   shiftSchedule: 换挡规律（数组）
+%   drivingCycle: 行驶循环名称
 %   根据传入的换挡规律、驾驶循环，构造对应的 Simulink.SimulationInput 并返回
+%   若传入多组换挡规律，则对应返回仿真输入对象数组
 
 arguments
-    shiftSchedule
+    shiftSchedule (1, :) ShiftSchedule
     drivingCycle (1, 1) {mustBeNonzeroLengthText} = "WLTC_class_2"
 end
 
-%% 初始化
+% 初始化
 modelName = "Dual_Clutch_Trans";
 shiftBlock = "Dual_Clutch_Trans/TCU/TCU/Shift Logic";
 load_system(modelName);
-simIn = Simulink.SimulationInput(modelName);
 
-%% 处理换挡规律
-% 创建Simulink.LookupTable对象备用
-% https://ww2.mathworks.cn/help/simulink/ug/configure-instance-specific-data-for-lookup-tables1.html
-upShiftLookupTable = Simulink.LookupTable;
-upShiftLookupTable.Table.Value = shiftSchedule.upSpds';
-upShiftLookupTable.Breakpoints(1).Value = shiftSchedule.upAPs;
-upShiftLookupTable.Breakpoints(2).Value = 1:height(shiftSchedule.upSpds);
-upShiftLookupTable.StructTypeInfo.Name = "UpShiftLookupTable";
-downShiftLookupTable = Simulink.LookupTable;
-downShiftLookupTable.Table.Value = shiftSchedule.downSpds';
-downShiftLookupTable.Breakpoints(1).Value = shiftSchedule.downAPs;
-downShiftLookupTable.Breakpoints(2).Value = ...
-    2:height(shiftSchedule.downSpds) + 1;
-downShiftLookupTable.StructTypeInfo.Name = "DownShiftLookupTable";
+taskCount = length(shiftSchedule); % 换挡参数数量（仿真任务数）
+simIn(1:taskCount) = Simulink.SimulationInput(modelName);
 
+for idx = 1:taskCount
+    % 处理换挡规律
+    % 创建Simulink.LookupTable对象备用
+    % https://ww2.mathworks.cn/help/simulink/ug/configure-instance-specific-data-for-lookup-tables1.html
+    upShiftLookupTable = Simulink.LookupTable;
+    upShiftLookupTable.Table.Value = shiftSchedule(idx).UpSpds';
+    upShiftLookupTable.Breakpoints(1).Value = shiftSchedule(idx).UpAPs;
+    upShiftLookupTable.Breakpoints(2).Value = 1:height(shiftSchedule(idx).UpSpds);
+    upShiftLookupTable.StructTypeInfo.Name = "UpShiftLookupTable";
+    downShiftLookupTable = Simulink.LookupTable;
+    downShiftLookupTable.Table.Value = shiftSchedule(idx).DownSpds';
+    downShiftLookupTable.Breakpoints(1).Value = shiftSchedule(idx).DownAPs;
+    downShiftLookupTable.Breakpoints(2).Value = ...
+        2:height(shiftSchedule(idx).DownSpds) + 1;
+    downShiftLookupTable.StructTypeInfo.Name = "DownShiftLookupTable";
 
-% 将新创建的查表对象设置到模型工作区中
-simIn = simIn.setVariable('upShiftLookupTable', upShiftLookupTable, ...
-    'Workspace', modelName);
-simIn = simIn.setVariable('downShiftLookupTable', downShiftLookupTable, ...
-    'Workspace', modelName);
+    % 将新创建的查表对象设置到模型工作区中
+    simIn(idx) = simIn(idx).setVariable('upShiftLookupTable', upShiftLookupTable, ...
+        'Workspace', modelName);
+    simIn(idx) = simIn(idx).setVariable('downShiftLookupTable', downShiftLookupTable, ...
+        'Workspace', modelName);
 
-% 通过修改模型引用的实例属性，使引用实例使用Dual_Clutch_Trans模型工作区中的查表
-instSpecParams = get_param(shiftBlock, 'InstanceParameters');
-instSpecParams(1).Value = 'downShiftLookupTable';
-instSpecParams(2).Value = 'upShiftLookupTable';
-simIn = simIn.setBlockParameter(shiftBlock, 'InstanceParameters', ...
-    instSpecParams);
+    % 通过修改模型引用的实例属性，使引用实例使用Dual_Clutch_Trans模型工作区中的查表
+    instSpecParams = get_param(shiftBlock, 'InstanceParameters');
+    instSpecParams(1).Value = 'downShiftLookupTable';
+    instSpecParams(2).Value = 'upShiftLookupTable';
+    simIn(idx) = simIn(idx).setBlockParameter(shiftBlock, 'InstanceParameters', ...
+        instSpecParams);
 
-%% 处理驾驶循环
-simIn = simIn.setVariable('DrivingCycle', drivingCycle, ...
-    'Workspace', modelName);
-simIn = simIn.setModelParameter('StopTime', "1477"); % FIXME 暂时写死
+    % 处理驾驶循环
+    drivingCycleDict = dictionary( ...
+        "WLTC_class_2", "1477", ...
+        "CLTC-C", "1800", ...
+        "AccelTest", "300");
+
+    simIn(idx) = simIn(idx).setVariable('DrivingCycle', drivingCycle, ...
+        'Workspace', modelName);
+    simIn(idx) = simIn(idx).setModelParameter('StopTime', drivingCycleDict(drivingCycle));
+
+end
 
 end
