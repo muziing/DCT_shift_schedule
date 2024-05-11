35c96e05107f8325293320704e70cd9ff8dd192d
muzing <muzi2001@foxmail.com>
2024-05-11 18:58:49 +0800

    Update project
    - 重命名 Data 目录下的脚本，使文件名风格统一；
    - 修改电机数据绘图脚本，完善模型数据绘图；

--- Data/plot_motor_data.m
+++ Data/plot_motor_data.m
@@ -16,7 +16,7 @@ figure('Name', "大轿车电机外特性曲线")
 hold on
 
 xlabel("转速 (rpm)")
-xlim([0, max(motSpeed)+400])
+xlim([0, max(motSpeed) + 400])
 xticks(0:800:max(motSpeed))
 
 yyaxis left
@@ -48,18 +48,18 @@ motTorArray = MotorData.Efficiency.RawData.Drive.torque;
 motEffArray = MotorData.Efficiency.RawData.Drive.systemEfficiency;
 
 % 设置网格与插值
-[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray),...
-    min(motTorArray)-20:2:max(motTorArray)+20);
+[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray), ...
+    min(motTorArray) - 20:2:max(motTorArray) + 20);
 motEff = griddata(motSpdArray, motTorArray, motEffArray, gridX, gridY);
 
 % 根据具体情况调整等高线层级与标签层级
 levelList = [70, 75, 80, 83, 86, 87:1:91, 91:0.5:95];
 labelLevelList = [70, 75, 80, 83, 86, 88, 90:1:93, 93:0.5:95];
 
-figure("Name","电机系统效率MAP图（电动）")
+figure("Name", "电机系统效率MAP图（电动）")
 colormap jet
 
-[C,h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
+[C, h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
 clabel(C, h, labelLevelList, 'LabelSpacing', 200)
 
 title("电机系统效率 MAP 图（电动）")
@@ -75,18 +75,18 @@ motTorArray = MotorData.Efficiency.RawData.Feed.torque;
 motEffArray = MotorData.Efficiency.RawData.Feed.systemEfficiency;
 
 % 设置网格与插值
-[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray),...
-    min(motTorArray)-20:1:max(motTorArray)+20);
+[gridX, gridY] = meshgrid(min(motSpdArray):10:max(motSpdArray), ...
+    min(motTorArray) - 20:1:max(motTorArray) + 20);
 motEff = griddata(motSpdArray, motTorArray, motEffArray, gridX, gridY);
 
 % 根据具体情况调整等高线层级
 levelList = [70, 75, 80, 83, 86, 88:1:90, 91:0.5:93.5, 94, 95];
 labelLevelList = [70, 75, 80, 83, 86, 88:1:93, 94, 95];
 
-figure("Name","电机系统效率MAP图（馈电）")
+figure("Name", "电机系统效率MAP图（馈电）")
 colormap jet
 
-[C,h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
+[C, h] = contourf(gridX, gridY, motEff, 'LevelList', levelList);
 clabel(C, h, labelLevelList, 'LabelSpacing', 200)
 
 title("电机系统效率 MAP 图（馈电）")
@@ -96,18 +96,33 @@ ylabel("转矩(N·m)")
 clear motSpdArray motTorArray motEffArray gridX gridY
 clear motEff levelList labelLevelList C h
 
-%% 数据处理（用于建模）
-% 以效率数据为依据，将电驱馈电整合，并以边界作为外特性，得到整理后的数据，
-% 用于 MappedMotor 模块的参数输入
+%% 模型数据绘图
+% 绘制 MappedMotor 模块实际使用数据
 
 % 外特性（与直接读取peek有微小区别）
 motSpdArray = unique(MotorData.Efficiency.RawData.Drive.speed)';
 motTorArray = zeros(1, length(motSpdArray));
 
 for index = 1:length(motSpdArray)
-    motTorArray(index) = max(MotorData.Efficiency.RawData.Drive{...
-        MotorData.Efficiency.RawData.Drive.speed == motSpdArray(index), 5});
+    motTorArray(index) = max( ...
+        MotorData.Efficiency.RawData.Drive{ ...
+                                           MotorData.Efficiency.RawData.Drive.speed == motSpdArray(index), 5});
 end
 
-figure("Name","MappedMotor 建模数据")
+% 效率
+[gridX, gridY] = meshgrid(MotorData.Efficiency.Drive.Speed, ...
+    MotorData.Efficiency.Drive.Torque);
+levelList = [70, 75, 80, 83, 86, 87:1:91, 91:0.5:95];
+labelLevelList = [70, 75, 80, 83, 86, 88, 90:1:93, 93:0.5:95];
+
+% 绘图
+figure("Name", "MappedMotor 建模数据")
+colormap jet
+hold on
 plot(motSpdArray, motTorArray)
+plot(motSpdArray, -motTorArray)
+[C, h] = contourf(gridX, gridY, MotorData.Efficiency.Drive.Eff, 'LevelList', levelList);
+clabel(C, h, labelLevelList, 'LabelSpacing', 200)
+[C, h] = contourf(gridX, -gridY, MotorData.Efficiency.Drive.Eff, 'LevelList', levelList);
+clabel(C, h, labelLevelList, 'LabelSpacing', 200)
+hold off
